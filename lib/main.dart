7fef446a1ec589a2f0a732c5324333a99979ef5d
chechna9 <ja_bouchouareb@esi.dz>
2023-01-02 23:54:29 +0100

    ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final counterProvider = StateProvider<int>((ref) => 0);
 
 void main() {
   runApp(
-    const MaterialApp(
-      title: 'Counter',
-      debugShowCheckedModeBanner: false,
-      home: MyApp(),
+    const ProviderScope(
+      child: MyApp(),
     ),
   );
 }
@@ -15,8 +16,58 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(),
+    return const MaterialApp(
+      title: 'Counter',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const CounterPage(),
+              ),
+            );
+          },
+          child: const Text('Go to Counter Page'),
+        ),
+      ),
+    );
+  }
+}
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Center(
+        child: Text(
+          '0',
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
